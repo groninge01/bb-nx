44e4eb18aefa01e9112dc03dbde6452a6ca4713e
groninge <groninge@gmail.com>
2023-08-19 13:29:30 +0200

    fix providers

--- apps/beets/app/providers.tsx
+++ apps/beets/app/providers.tsx
@@ -1,12 +1,14 @@
-'use client';
-
 import { TokensProvider, Web3Provider } from '@bb-nx/shared/modules';
 import { ApolloProviderWrapper, ThemeProvider } from '@bb-nx/shared/services';
+import { COLOR_MODE_STORAGE_KEY } from '@bb-nx/shared/constants';
 import { ReactNode } from 'react';
+import { cookies } from 'next/headers';
 
 export function Providers({ children }: { children: ReactNode }) {
+  const initialColorMode = cookies().get(COLOR_MODE_STORAGE_KEY)?.value;
+
   return (
-    <ThemeProvider>
+    <ThemeProvider initialColorMode={initialColorMode}>
       <Web3Provider>
         <ApolloProviderWrapper>
           <TokensProvider>{children}</TokensProvider>
